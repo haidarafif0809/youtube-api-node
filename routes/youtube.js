17011426ccf95da1bc16df778f18cbcba8fb9a44
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-06-30 09:42:38 +0700

    Initial Commit - Bismillah

---
+++ routes/youtube.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube('AIzaSyBIPs4-L8KAm1oymrXvAu-KzdxUKF7LqkQ');
+/* GET users listing. */
+
+router.get('/:id', function(req, res, next) {
+  const id = req.params.id
+  youtube.getVideoByID(id)
+   .then(results => {
+     res.json(results)
+   })
+ .catch(console.error);
+});
+
+router.get('/', function(req, res, next) {
+  youtube.search('')
+   .then(results => {
+     res.json(results)
+   })
+ .catch(console.error);
+});
+
+router.get('/search/:id', function(req, res, next) {
+  const id = req.params.id
+  youtube.search(id)
+   .then(results => {
+     res.json(results)
+   })
+ .catch(console.error);
+});
+
+
+module.exports = router;
