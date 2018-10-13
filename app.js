5fd1567fe35f3af906f37a41c84fd8dfcf262f91
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-10-13 10:22:45 +0700

    Add cors

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var youtube = require('./routes/youtube');
+const cors = require('cors')
+
 
 var app = express();
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
